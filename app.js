fc321e22c2be1abeefd75673d2e8118206e74af0
Gary White <glw494@gmail.com>
2021-10-20 13:48:01 -0400

    UI draft

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
+// Game script
+
 let computerScore = 0;
 let playerScore = 0;
 
 function computerPlay() {
-    const choices = ["rock","paper","scissors"]
+    const choices = ["Rock","Paper","Scissors"]
     const computerSelection = choices[Math.floor(Math.random() * choices.length)]
-    
-    return computerSelection.toLowerCase();
+
+    return computerSelection
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    computerSelection = computerPlay();
+
+    if (computerScore == 5) {
+        gameStatus.innerHTML = "Game over! Computer wins. Refresh to play again."
+        return
+    }
+
+    if (playerScore == 5) {
+        gameStatus.innerHTML = "You win the game! Refresh to play again."
+        return
+    }
 
     if (playerSelection == computerSelection) {
-        console.log("It's a draw!")
+        gameStatus.innerHTML = "It's a draw!"
     }
-    else if ((playerSelection == "rock" && computerSelection == "paper") || 
-            (playerSelection == "paper" && computerSelection == "scissors") || 
-            (playerSelection == "scissors" && computerSelection == "rock")) {
+    else if ((playerSelection == "Rock" && computerSelection == "Paper") || 
+            (playerSelection == "Paper" && computerSelection == "Scissors") || 
+            (playerSelection == "Scissors" && computerSelection == "Rock")) {
             
-            console.log(`"You lose! ${computerSelection} beats ${playerSelection}"`)
-
             computerScore++
+            gameStatus.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}.`
     }
-    else if ((playerSelection == "rock" && computerSelection == "scissors") || 
-            (playerSelection == "paper" && computerSelection == "rock") || 
-            (playerSelection == "scissors" && computerSelection == "paper")) {
+    else if ((playerSelection == "Rock" && computerSelection == "Scissors") || 
+            (playerSelection == "Paper" && computerSelection == "Rock") || 
+            (playerSelection == "Scissors" && computerSelection == "Paper")) {
             
-            console.log(`"You win! ${playerSelection} beats ${computerSelection}`)
-
             playerScore++
+            gameStatus.innerHTML = `You win! ${playerSelection} beats ${computerSelection}.`
     }
+
+    playerScoreDisplay.innerHTML = `Player: ${playerScore}`;
+    playerChoiceDisplay.innerHTML = playerSelection
+
+    computerScoreDisplay.innerHTML = `Computer: ${computerScore}`;
+    computerChoiceDisplay.innerHTML = computerSelection
 }
+
+// UI
+
+let playerScoreDisplay = document.getElementById("player-score");
+let playerChoiceDisplay = document.getElementById("player-choice");
+let computerScoreDisplay = document.getElementById("computer-score");
+let computerChoiceDisplay = document.getElementById("computer-choice");
+
+let rockButton = document.getElementById("rock-button");
+rockButton.addEventListener('click', () => playRound("Rock", computerPlay()));
+
+let paperButton = document.getElementById("paper-button");
+paperButton.addEventListener('click', () => playRound("Paper", computerPlay()));
+
+let scissorsButton = document.getElementById("scissors-button");
+scissorsButton.addEventListener('click', () => playRound("Scissors", computerPlay()))
+
+let gameStatus = document.getElementById("game-status");
